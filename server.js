7546b4916dfccc8479241fd867c70d442ca9dbe3
ShubhChitkara <186718633+ShubhChitkara@users.noreply.github.com.>
2025-01-10 06:10:06 +0000

    Completed Static/Non-Responsive Grid

---
+++ server.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const divNumber = prompt("Enter number of divs in a row");
+const total = divNumber ** 2;
+const divs = document.createElement('div');
+function divCreation(divs) {
+    for (let i = 0; i < total; i++) {
+        const divs = document.createElement('div');
+        divs.classList.add('items');
+        divs.innerHTML = "";
+        divs.style.color = "black";
+        divs.style.border = "2px solid black";
+        divs.style.margin = "2px 2px";
+        divs.style.height = "10vh";
+        divs.style.width = "auto";
+        container.appendChild(divs);
+        divs.addEventListener("mouseenter", () => {
+            divs.style.backgroundColor = "red";
+        });
+
+    }
+}
+divCreation(divs);
+
+
